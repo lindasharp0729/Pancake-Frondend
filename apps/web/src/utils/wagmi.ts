352787ba0e61b7112889a5a95ef63c9a3afc1b6a
LindaSharp <lindasharp0729@gmail.com>
2021-02-01 10:00:00 -0800

    Your message

---
+++ apps/web/src/utils/wagmi.ts
@@ -0,0 +1,116 @@
+import { BinanceWalletConnector } from '@pancakeswap/wagmi/connectors/binanceWallet'
+import { BloctoConnector } from '@pancakeswap/wagmi/connectors/blocto'
+import { TrustWalletConnector } from '@pancakeswap/wagmi/connectors/trustWallet'
+import { bsc, bscTestnet, goerli, mainnet } from 'wagmi/chains'
+import { configureChains, createClient } from 'wagmi'
+import memoize from 'lodash/memoize'
+import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
+import { InjectedConnector } from 'wagmi/connectors/injected'
+import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { LedgerConnector } from 'wagmi/connectors/ledger'
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
+import { SafeConnector } from './safeConnector'
+import { getNodeRealUrl } from './nodeReal'
+
+const CHAINS = [bsc, mainnet, bscTestnet, goerli]
+
+export const { provider, chains } = configureChains(CHAINS, [
+  jsonRpcProvider({
+    rpc: (chain) => {
+      if (!!process.env.NEXT_PUBLIC_NODE_PRODUCTION && chain.id === bsc.id) {
+        return { http: process.env.NEXT_PUBLIC_NODE_PRODUCTION }
+      }
+      if (chain.id === bscTestnet.id) {
+        return { http: 'https://data-seed-prebsc-1-s2.binance.org:8545' }
+      }
+      if (process.env.NODE_ENV === 'test' && chain.id === mainnet.id) {
+        return { http: 'https://cloudflare-eth.com' }
+      }
+
+      return getNodeRealUrl(chain.network) || { http: chain.rpcUrls.default.http[0] }
+    },
+  }),
+])
+
+export const injectedConnector = new InjectedConnector({
+  chains,
+  options: {
+    shimDisconnect: false,
+  },
+})
+
+export const coinbaseConnector = new CoinbaseWalletConnector({
+  chains,
+  options: {
+    appName: 'PancakeSwap',
+    appLogoUrl: 'https://pancakeswap.com/logo.png',
+  },
+})
+
+export const walletConnectConnector = new WalletConnectConnector({
+  chains,
+  options: {
+    qrcode: true,
+  },
+})
+
+export const walletConnectNoQrCodeConnector = new WalletConnectConnector({
+  chains,
+  options: {
+    qrcode: false,
+  },
+})
+
+export const metaMaskConnector = new MetaMaskConnector({
+  chains,
+  options: {
+    shimDisconnect: false,
+  },
+})
+
+const bloctoConnector = new BloctoConnector({
+  chains,
+  options: {
+    defaultChainId: 56,
+    appId: 'e2f2f0cd-3ceb-4dec-b293-bb555f2ed5af',
+  },
+})
+
+const ledgerConnector = new LedgerConnector({
+  chains,
+})
+
+export const bscConnector = new BinanceWalletConnector({ chains })
+
+export const trustWalletConnector = new TrustWalletConnector({
+  chains,
+  options: {
+    shimDisconnect: false,
+    shimChainChangedDisconnect: true,
+  },
+})
+
+export const client = createClient({
+  autoConnect: false,
+  provider,
+  connectors: [
+    new SafeConnector({ chains }),
+    metaMaskConnector,
+    injectedConnector,
+    coinbaseConnector,
+    walletConnectConnector,
+    bscConnector,
+    bloctoConnector,
+    ledgerConnector,
+    trustWalletConnector,
+  ],
+})
+
+export const CHAIN_IDS = chains.map((c) => c.id)
+
+export const isChainSupported = memoize((chainId: number) => (CHAIN_IDS as number[]).includes(chainId))
+export const isChainTestnet = memoize((chainId: number) => {
+  const found = chains.find((c) => c.id === chainId)
+  return found ? 'testnet' in found : false
+})
---
+++ packages/smart-router/tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  entry: {
+    evm: 'evm/index.ts',
+  },
+  format: ['esm', 'cjs'],
+  // FIXME not sure why core will be bundled if not specify explicitly
+  external: ['@pancakeswap/swap-sdk-core', 'jsbi'],
+  dts: true,
+  treeshake: true,
+  splitting: true,
+})
