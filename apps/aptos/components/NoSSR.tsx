e154fe61a33d772d064817d2fd16bfc095dc6c78
LindaSharp <lindasharp0729@gmail.com>
2021-04-18 17:40:00 +0300

    update

--- apps/aptos/components/NoSSR.tsx
+++ apps/aptos/components/NoSSR.tsx
@@ -5,10 +5,8 @@ interface NoSSRProps {
   children?: React.ReactNode
   loader?: React.ReactNode
 }
-
 const NoSSR: React.FC<NoSSRProps> = ({ children, loader }) => {
   const isMounted = useIsMounted()
-
   return <>{isMounted ? children : loader ?? null}</>
 }
 
--- apps/aptos/components/TokenImage/index.tsx
+++ apps/aptos/components/TokenImage/index.tsx
@@ -5,17 +5,14 @@ import {
   TokenImage as UIKitTokenImage,
   ImageProps,
 } from '@pancakeswap/uikit'
-
 interface TokenPairImageProps extends Omit<UIKitTokenPairImageProps, 'primarySrc' | 'secondarySrc'> {
   primaryToken: Currency
   secondaryToken: Currency
 }
-
 const getImageUrlFromToken = (token: Currency) => {
   const address = token?.isNative ? token.wrapped.address : token.address
   return `https://tokens.pancakeswap.finance/images/aptos/${address}.png`
 }
-
 export const TokenPairImage: React.FC<React.PropsWithChildren<TokenPairImageProps>> = ({
   primaryToken,
   secondaryToken,
@@ -29,11 +26,9 @@ export const TokenPairImage: React.FC<React.PropsWithChildren<TokenPairImageProp
     />
   )
 }
-
 interface TokenImageProps extends ImageProps {
   token: Currency
 }
-
 export const TokenImage: React.FC<React.PropsWithChildren<TokenImageProps>> = ({ token, ...props }) => {
   return <UIKitTokenImage src={getImageUrlFromToken(token)} {...props} />
 }
